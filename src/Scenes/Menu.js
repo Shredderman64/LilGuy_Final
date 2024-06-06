f09862568b0d21cbeb676d418df90248b7db384c
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-06-06 13:25:51 -0700

    Made changes to foreground

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -6,15 +6,15 @@ class Menu extends Phaser.Scene {
     create() {
         this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height,
         "menu_background").setOrigin(0).setScale(2.0);
-        this.foreground = this.add.tileSprite(0, game.config.height - 36, game.config.width,
-        36, "menu_foreground").setOrigin(0).setScale(2.0);
+        this.foreground = this.add.tileSprite(0, game.config.height - 72, game.config.width,
+        72, "menu_foreground").setOrigin(0).setScale(4.0);
 
         this.startGame = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     }
 
     update() {
-        this.background.tilePositionX += 1;
-        this.foreground.tilePositionX += 2;
+        this.background.tilePositionX += 0.5;
+        this.foreground.tilePositionX += 1;
 
         if (Phaser.Input.Keyboard.JustDown(this.startGame))
             this.scene.start("levelOneScene");
