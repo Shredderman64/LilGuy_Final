5b26b8f1efcef5d7444718a8f288035737b27015
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-06-09 16:58:47 -0700

    Added comments

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -6,13 +6,17 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
+        // scrolling background
         this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height,
         "menu_background").setOrigin(0).setScale(2.0);
         this.foreground = this.add.tileSprite(0, game.config.height - 72, game.config.width,
         72, "menu_foreground").setOrigin(0).setScale(4.0);
 
+        // title
         this.title = this.add.bitmapText(game.config.width / 2, game.config.height / 2, "retro",
         "LIL GUY", 100).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD);
+
+        // options
         this.options = [];
         this.options.push(
             this.add.bitmapText(game.config.width / 2, (game.config.height / 2) + 60, "retro",
@@ -23,6 +27,7 @@ class Menu extends Phaser.Scene {
             "Press C to view credits", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
         );
 
+        // credits
         this.credits = [];
         this.credits.push(
             this.add.bitmapText(game.config.width / 2, (game.config.height / 2) - 60, "retro",
@@ -49,6 +54,7 @@ class Menu extends Phaser.Scene {
             "https://www.dafont.com/retro-gaming.font", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
         )
 
+        // hide credits
         for (let credit of this.credits)
             credit.setVisible(false);
 
@@ -63,6 +69,7 @@ class Menu extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(this.startGame))
             this.scene.start("levelOneScene");
 
+        // show credits on key press
         if (Phaser.Input.Keyboard.JustDown(this.viewCredits)) {
             if (!this.switchPage) {
                 this.title.setVisible(false);
