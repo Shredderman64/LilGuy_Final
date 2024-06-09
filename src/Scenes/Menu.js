665e51fa1789d0a63c33f0ad2109386dfd08e812
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-06-09 16:53:23 -0700

    Added credits and options to menu

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -1,6 +1,8 @@
 class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
+
+        this.switchPage = false;
     }
 
     create() {
@@ -9,7 +11,49 @@ class Menu extends Phaser.Scene {
         this.foreground = this.add.tileSprite(0, game.config.height - 72, game.config.width,
         72, "menu_foreground").setOrigin(0).setScale(4.0);
 
+        this.title = this.add.bitmapText(game.config.width / 2, game.config.height / 2, "retro",
+        "LIL GUY", 100).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD);
+        this.options = [];
+        this.options.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) + 60, "retro",
+            "Press ENTER to start game", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        );
+        this.options.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) + 90, "retro",
+            "Press C to view credits", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        );
+
+        this.credits = [];
+        this.credits.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) - 60, "retro",
+            "A Game By", 40).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        );
+        this.credits.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) - 30, "retro",
+            "Kellum Inglin & Garrett Yu", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        )
+        this.credits.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2), "retro",
+            "Audio and Art Assets", 40).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        )
+        this.credits.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) + 30, "retro",
+            "Kenney Assets", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        )
+        this.credits.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) + 60, "retro",
+            "Retro Gaming Font", 40).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        )
+        this.credits.push(
+            this.add.bitmapText(game.config.width / 2, (game.config.height / 2) + 90, "retro",
+            "https://www.dafont.com/retro-gaming.font", 25).setOrigin(0.5).setBlendMode(Phaser.BlendModes.ADD)
+        )
+
+        for (let credit of this.credits)
+            credit.setVisible(false);
+
         this.startGame = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        this.viewCredits = this.input.keyboard.addKey("C");
     }
 
     update() {
@@ -18,5 +62,23 @@ class Menu extends Phaser.Scene {
 
         if (Phaser.Input.Keyboard.JustDown(this.startGame))
             this.scene.start("levelOneScene");
+
+        if (Phaser.Input.Keyboard.JustDown(this.viewCredits)) {
+            if (!this.switchPage) {
+                this.title.setVisible(false);
+                for (let option of this.options)
+                    option.setVisible(false);
+                for (let credit of this.credits)
+                    credit.setVisible(true);
+                this.switchPage = true;
+            } else {
+                this.title.setVisible(true);
+                for (let option of this.options)
+                    option.setVisible(true);
+                for (let credit of this.credits)
+                    credit.setVisible(false);
+                this.switchPage = false;
+            }
+        }
     }
 }
